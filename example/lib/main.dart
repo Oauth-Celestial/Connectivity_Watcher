4a33612d102cca9a9daabf35d5ba2c802de1e9da
Ravi Pai <ravi.findit@gmail.com>
2024-12-30 14:29:30 +0530

    Release 3.0.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
 import 'package:connectivity_watcher/connectivity_watcher.dart';
-import 'package:connectivity_watcher/screens/custom_no_internet.dart';
-import 'package:dio/dio.dart';
+
 import 'package:example/no_internet.dart';
 import 'package:flutter/material.dart';
 
@@ -19,7 +17,7 @@ class MyApp extends StatelessWidget {
     return ZoConnectivityWrapper(
       /// connectivityStyle: NoConnectivityStyle.CUSTOM,
       navigationKey: navigatorKey,
-      connectivityStyle: NoConnectivityStyle.CUSTOMALERT,
+      connectivityStyle: NoConnectivityStyle.ALERT,
       customAlert: AlertDialog(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16.0),
@@ -133,24 +131,22 @@ class _LoginDemoState extends State<LoginDemo> {
                   color: Colors.blue, borderRadius: BorderRadius.circular(20)),
               child: MaterialButton(
                 onPressed: () async {
-                  ZoConnectivityWatcher().makeApiCall(apiCall: (status) async {
-                    if (status) {
-                      Dio dio = Dio();
+                  // ZoConnectivityWatcher().makeApiCall(apiCall: (status) async {
+                  //   if (status) {
+                  //     Dio dio = Dio();
 
-                      dio.interceptors.add(CurlInterceptor());
+                  //     dio.interceptors.add(CurlInterceptor());
 
-                      Response data = await dio.post(
-                          "https://jsonplaceholder.typicode.com/posts",
-                          data: {
-                            "title": 'foo',
-                            "body": 'bar',
-                            "userId": 1,
-                          });
-                    }
-                  });
-
-                  // Navigator.of(context)
-                  //     .push(MaterialPageRoute(builder: (_) => LoginDemoTwo()));
+                  //     Response data = await dio.post(
+                  //         "https://jsonplaceholder.typicode.com/posts",
+                  //         data: {
+                  //           "title": 'foo',
+                  //           "body": 'bar',
+                  //           "userId": 1,
+                  //         });
+                  //   }
+                  // });
+                  print(ZoConnectivityWatcher().isInternetAvailable);
                 },
                 child: Text(
                   'Login',
@@ -161,108 +157,42 @@ class _LoginDemoState extends State<LoginDemo> {
             SizedBox(
               height: 130,
             ),
-            Text('New User? Create Account')
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-
-    super.dispose();
-  }
-}
-
-class LoginDemoTwo extends StatefulWidget {
-  @override
-  _LoginDemoTwoState createState() => _LoginDemoTwoState();
-}
-
-class _LoginDemoTwoState extends State<LoginDemoTwo> {
-  late StreamSubscription<ConnectivityWatcherStatus> subscription;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    ZoConnectivityWatcher().subscribeToConnectivityChange(
-        subscriptionCallback: ((stream) {
-      subscription = stream.listen((event) {
-        print(
-            "Internet Status on screen two  ${event == ConnectivityWatcherStatus.connected}");
-      });
-    }));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Login Page"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 200,
-            ),
-            Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    hintText: 'Enter valid email id as abc@gmail.com'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15, bottom: 0),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter secure password'),
-              ),
-            ),
-            MaterialButton(
-              onPressed: () async {
-                bool internetStatus =
-                    await ZoConnectivityWatcher().getConnectivityStatus();
-                print(internetStatus);
+            Text('New User? Create Account'),
+            ZoNetworkAware(
+              builder: (context, status) {
+                if (status == ConnectivityWatcherStatus.connected) {
+                  return Container(
+                    height: 50,
+                    width: 250,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: MaterialButton(
+                      onPressed: () async {},
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.black, fontSize: 25),
+                      ),
+                    ),
+                  );
+                } else {
+                  return Container(
+                    height: 50,
+                    width: 250,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: MaterialButton(
+                      onPressed: () async {},
+                      child: Text(
+                        'Disconnected',
+                        style: TextStyle(color: Colors.black, fontSize: 25),
+                      ),
+                    ),
+                  );
+                }
               },
-              child: Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
             ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-              child: MaterialButton(
-                onPressed: () async {
-                  bool hasInternet =
-                      await ZoConnectivityWatcher().getConnectivityStatus();
-                  print(hasInternet);
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 130,
-            ),
-            Text('New User? Create Account')
           ],
         ),
       ),
@@ -272,7 +202,7 @@ class _LoginDemoTwoState extends State<LoginDemoTwo> {
   @override
   void dispose() {
     // TODO: implement dispose
-    subscription.cancel();
+
     super.dispose();
   }
 }
